f8380179202ea2d4ad1edd46ee3f6a8c6d9736a8
Ziqian Lin <51812297+TheHiddenObserver@users.noreply.github.com>
2026-07-27 23:42:49 +0800

    Install backend-native Cox fit boundary

--- statgpu/survival/__init__.py
+++ statgpu/survival/__init__.py
@@ -9,6 +9,10 @@ Survival analysis models.
 """
 
 from ._cox import CoxPH
+from ._cox_fit_adapter import install_coxph_fit_adapter
 from ._cox_cv import CoxPHCV
+
+install_coxph_fit_adapter(CoxPH)
+del install_coxph_fit_adapter
 
 __all__ = ['CoxPH', 'CoxPHCV']
