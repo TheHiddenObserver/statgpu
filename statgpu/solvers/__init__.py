c2daedde720c102fea0d28ee87c9fe7dc8725c30
Ziqian Lin <51812297+TheHiddenObserver@users.noreply.github.com>
2026-09-16 15:24:16 +0800

    fix: close unsupported Quantile solver routes

--- statgpu/solvers/__init__.py
+++ statgpu/solvers/__init__.py
@@ -25,12 +25,10 @@ from . import _adaptive_group_lipschitz_contract as _adaptive_group_lipschitz_co
 
 from ._convergence import ConvergenceWarning
 from ._fista import fista_solver
-from ._fista_bb import fista_bb_solver
 from ._fista_lla_group_contract import fista_lla_path
 from ._newton import newton_solver
 from ._proximal_newton import proximal_newton_solver
 from ._proximal_irls_quantile import proximal_irls_quantile_solver
 from ._quantile_cd import quantile_cd_solver
-from ._lbfgs import lbfgs_solver
 from ._lbfgs_b import lbfgs_b_solver
-from ._quantile_admm_guard import admm_solver
+from ._quantile_solver_guard import admm_solver, fista_bb_solver, lbfgs_solver
