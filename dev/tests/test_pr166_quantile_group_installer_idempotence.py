744a5fa76383105e05112f4fada8e3aa4aea49d7
Ziqian Lin <51812297+TheHiddenObserver@users.noreply.github.com>
2026-09-17 23:52:47 +0800

    test: cover quantile group CV score installer idempotence

--- dev/tests/test_pr166_quantile_group_installer_idempotence.py
+++ dev/tests/test_pr166_quantile_group_installer_idempotence.py
@@ -14,11 +14,13 @@ def test_quantile_group_installer_reload_preserves_fit_and_cv_wrappers():
     before_fit = _fit_mixin._PenalizedFitMixin._fit_loss_backend
     before_fold = CV._cv_fold_general
     before_refit = CV._refit_best
+    before_scores = CV._compute_cv_scores
 
     importlib.reload(contract)
 
     assert _fit_mixin._PenalizedFitMixin._fit_loss_backend is before_fit
     assert CV._cv_fold_general is before_fold
     assert CV._refit_best is before_refit
+    assert CV._compute_cv_scores is before_scores
     assert getattr(before_fit, contract._MARKER, False)
     assert getattr(CV, contract._CV_MARKER, False)
